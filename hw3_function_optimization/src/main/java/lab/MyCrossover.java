21dd100ee3d7dbeccb6498004e50ae2717cbd321
traptrip <aletni@gmail.com>
2023-03-11 23:31:44 +0300

    add hw3

---
+++ hw3_function_optimization/src/main/java/lab/MyCrossover.java
@@ -0,0 +1,49 @@
+package lab;
+
+import org.uncommons.watchmaker.framework.operators.AbstractCrossover;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class MyCrossover extends AbstractCrossover<double[]> {
+    protected MyCrossover() {
+        super(1);
+    }
+
+    protected List<double[]> mate(double[] p1, double[] p2, int i, Random random) {
+        ArrayList children = new ArrayList();
+
+        // your implementation:
+        double alpha = random.nextDouble();
+        double[] child = simpleCrossover(p1, p2, alpha, random);
+
+        children.add(child);
+        children.add(p1);
+        children.add(p2);
+
+        return children;
+    }
+    public static double[] simpleCrossover(double[] parent1, double[] parent2, double alpha, Random random) {
+        double[] child = new double[parent1.length];
+        double threshold = 0.4;
+        for (int i = 0; i < parent1.length; i++) {
+            if (random.nextDouble() < threshold) {
+                // cross parents
+                child[i] = alpha * parent1[i] + (1 - alpha) * parent2[i];
+            } else {
+                // chose one of parents
+                if (random.nextDouble() < 0.5) {
+                    child[i] = parent1[i];
+                }
+                else {
+                    child[i] = parent2[i];
+                }
+            }
+        }
+        return child;
+    }
+}
+
+
---
+++ hw3_function_optimization/src/main/java/lab/MyMutation.java
@@ -0,0 +1,45 @@
+package lab;
+
+import org.uncommons.watchmaker.framework.EvolutionaryOperator;
+
+import java.util.List;
+import java.util.Random;
+
+public class MyMutation implements EvolutionaryOperator<double[]> {
+    public List<double[]> apply(List<double[]> population, Random random) {
+        // initial population
+        // need to change individuals, but not their number!
+
+        // your implementation:
+        double mutationThreshold = 0.5;
+        double sigma = 0.8;
+        int numIndividuals = population.size();
+        for (int i = 0; i < numIndividuals; i++) {
+            double[] individual = population.get(i);
+            double[] mutatedIndividual = gaussianMutation(individual, random, mutationThreshold, sigma);
+            population.set(i, mutatedIndividual);
+        }
+
+        //result population
+        return population;
+    }
+
+    public static double[] gaussianMutation(double[] individual, Random random, double mutationThreshold, double sigma) {
+        int numGenes = individual.length;
+        double[] mutatedIndividual = new double[numGenes];
+        int maxMutations = (int) (0.4 * numGenes);
+        int cntMutations = 0;
+        for (int i = 0; i < numGenes; i++) {
+            if ((cntMutations < maxMutations) & (random.nextDouble() < mutationThreshold)) {
+                // add noise
+                mutatedIndividual[i] = individual[i] + sigma * random.nextDouble() * random.nextGaussian();
+                // clamp to [-5, 5]
+                mutatedIndividual[i] = Math.min(Math.max(mutatedIndividual[i], -5), 5);
+                cntMutations++;
+            } else {
+                mutatedIndividual[i] = individual[i];
+            }
+        }
+        return mutatedIndividual;
+    }
+}
